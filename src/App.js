cf00013952c611fefd94caafb4c814443297ddd2
sene03 <hansel0311@gmail.com>
2025-08-05 08:54:04 +0900

    theme

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import "./App.css";
 import styled from "styled-components";
 
-const Box = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
-const Box2 = styled(Box)`
-  background-color: red;
-`;
-
-const Text = styled.span`
-  color: ${(props) => props.textColor};
-  span {
-    color: blue;
-    &:hover {
-      color: red;
-    }
-  }
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
-    <>
-      <Box>
-        <Text textColor="black">
-          asdf
-          <span>this is span</span>
-        </Text>
-      </Box>
-      <Box2></Box2>
-    </>
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
   );
 }
 
