8317007213eb78a9a4916e3022cb92f2788af6b4
sene03 <hansel0311@gmail.com>
2025-08-11 10:41:17 +0900

    basic setting for theme and recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme.ts";
+import { RecoilRoot } from "recoil";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </StrictMode>
+);
